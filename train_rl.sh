bc2f9f3e1eeddbfb8a90ab880823bc4f60cbffc0
dang <dang.nguyen14082002@gmail.com>
2024-04-22 22:31:16 +0700

    .

---
+++ train_rl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+accelerate launch \
+    --config_file=examples/accelerate_configs/multi_gpu.yaml \
+    --num_processes {NUM_GPUS} \
+    trainer/rl.py \
+    --all_arguments_of_the_script
