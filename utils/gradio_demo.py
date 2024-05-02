de459696d1409f2ae488eec380653ba9b7f5dd5b
dang <dang.nguyen14082002@gmail.com>
2024-05-02 23:38:18 +0700

    .

--- utils/gradio_demo.py
+++ utils/gradio_demo.py
@@ -28,12 +28,10 @@ def run_demo():
     agrs = load_args()
 
     model, tokenizer = load_model(model_name_or_path=agrs.model_name_or_path, token=agrs.token)
-
+    pipe = lambda context, answer : infer(model=model, tokenizer=tokenizer, context=context, answer=answer)
     demo = gr.Interface(
-        fn=infer,
+        fn=pipe,
         inputs=[
-            model,
-            tokenizer,
             gr.Textbox(
                 value="Năm 1871, Đức trở thành một quốc gia dân tộc khi hầu hết các quốc gia Đức thống nhất trong Đế quốc Đức do Phổ chi phối. Sau Chiến tranh thế giới thứ nhất và Cách mạng Đức 1918-1919, Đế quốc này bị thay thế bằng Cộng hòa Weimar theo chế độ nghị viện. Chế độ độc tài quốc xã được hình thành vào năm 1933, dẫn tới Chiến tranh thế giới thứ hai và một nạn diệt chủng. Sau một giai đoạn Đồng Minh chiếm đóng, hai nước Đức được thành lập: Cộng hòa Liên bang Đức và Cộng hòa Dân chủ Đức. Năm 1990, quốc gia được tái thống nhất.",
                 label="Context",
