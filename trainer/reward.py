1f1ccc58736910396834ab9317a2da39160069fc
dang <dang.nguyen14082002@gmail.com>
2024-05-15 17:20:24 +0700

    .

--- trainer/reward.py
+++ trainer/reward.py
@@ -263,6 +263,40 @@ if __name__ == "__main__":
     ################
     # Training
     ################
+    class customTrainer(RewardTrainer):
+        def compute_loss(
+            self,
+            model: Union[PreTrainedModel, nn.Module],
+            inputs: Dict[str, Union[torch.Tensor, Any]],
+            return_outputs=False,
+        ) -> Union[torch.Tensor, Tuple[torch.Tensor, Dict[str, torch.Tensor]]]:
+            if not self.use_reward_data_collator:
+                warnings.warn(
+                    "The current compute_loss is implemented for RewardDataCollatorWithPadding,"
+                    " if you are using a custom data collator make sure you know what you are doing or"
+                    " implement your own compute_loss method."
+                )
+            rewards_chosen = model(
+                input_ids=inputs["input_ids_chosen"],
+                attention_mask=inputs["attention_mask_chosen"],
+                return_dict=True,
+            )["logits"]
+            rewards_rejected = model(
+                input_ids=inputs["input_ids_rejected"],
+                attention_mask=inputs["attention_mask_rejected"],
+                return_dict=True,
+            )["logits"]
+            # calculate loss, optionally modulate with margin
+            loss = (rewards_chosen - rewards_rejected).mean()
+
+            if return_outputs:
+                return loss, {
+                    "rewards_chosen": rewards_chosen,
+                    "rewards_rejected": rewards_rejected,
+                }
+            return loss
+
+
     reward_config.remove_unused_columns=False
     trainer = RewardTrainer(
         model=model,
