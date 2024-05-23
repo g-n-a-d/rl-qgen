601c236dfce2cea56e373f12c72000439b4fa815
dang <dang.nguyen14082002@gmail.com>
2024-05-23 23:51:29 +0700

    .

--- train_clm.sh
+++ train_clm.sh
@@ -9,23 +9,21 @@ accelerate launch \
     --answer_column answer \
     --train_file ./data/processed/train.jsonl \
     --validation_file ./data/processed/dev.jsonl \
-    --test_file ./data/processed/test.jsonl \
-    --max_source_length 1024 \
+    --max_source_length 2048 \
     --output_dir ./outputs/ \
     --do_train \
     --do_eval \
     --evaluation_strategy steps \
-    --eval_steps 400 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
-    --learning_rate 3e-4 \
-    --num_train_epochs 8 \
+    --eval_steps 200 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 4 \
     --logging_strategy steps \
     --logging_steps 25 \
     --save_strategy steps \
-    --save_step 400 \
+    --save_step 200 \
     --save_total_limit 5 \
     --load_best_model_at_end True \
-    --report_to none \
-    --predict_with_generate True
+    --report_to none
