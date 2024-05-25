b6654cee3d23c973b1231a355bee41a052a1dc14
dang <dang.nguyen14082002@gmail.com>
2024-05-26 00:35:57 +0700

    .

--- trainer/clm.py
+++ trainer/clm.py
@@ -224,7 +224,7 @@ def main():
             eval_dataset=eval_dataset if training_args.do_eval else None,
             tokenizer=tokenizer,
             data_collator=data_collator,
-            compute_metrics=compute_metrics,
+            # compute_metrics=compute_metrics,
         )
 
 
