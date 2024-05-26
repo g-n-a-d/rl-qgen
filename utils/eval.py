b06195c924abeddda5ecda2061b6c0facdb12105
dang <dang.nguyen14082002@gmail.com>
2024-05-26 17:20:15 +0700

    .

--- utils/eval.py
+++ utils/eval.py
@@ -27,7 +27,6 @@ args = parser.parse_args()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-print(tokenizer.padding_side)
 model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path).to(device)
 if args.adapter_name_or_path:
     adapter = model.load_adapter(args.adapter_name_or_path)
@@ -58,9 +57,10 @@ with jsonlines.open(args.eval_filename, mode="r") as fr, jsonlines.open(args.out
             top_k=args.top_k,
             top_p=args.top_p,
         )
-        print(i)
-        print(preds)
         outputs = tokenizer.batch_decode(preds, skip_special_tokens=True)
+        print(i)
+        for o in outputs:
+            print(o)
         for ii in range(args.eval_batch_size):
             score = scorer.score(text[ii]["question"], outputs[ii].split("### Câu hỏi:")[1])
             rougeL_pre.append(score["rougeL"].precision)
