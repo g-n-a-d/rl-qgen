57a4e1f9d223c2204da490b1fd6b59805ac09a67
dang <dang.nguyen14082002@gmail.com>
2024-05-26 12:14:15 +0700

    .

--- trainer/clm.py
+++ trainer/clm.py
@@ -209,7 +209,7 @@ def main():
         if isinstance(preds, tuple):
             preds = preds[0]
         # Replace -100s used for padding as we can't decode them
-        preds = np.where(preds != -100, preds, tokenizer.pad_token_id)
+        preds = np.where(labels != -100, preds, tokenizer.pad_token_id)
         decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
