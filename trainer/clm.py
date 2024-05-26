1c5cf0876156f42b56d71918e8f6baa22079a913
dang <dang.nguyen14082002@gmail.com>
2024-05-26 11:53:19 +0700

    .

--- trainer/clm.py
+++ trainer/clm.py
@@ -213,6 +213,7 @@ def main():
         decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        print(decoded_preds, decoded_labels)
 
         # Some simple post-processing
         decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
