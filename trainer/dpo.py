2123c9c4caabfb42b59b5ce0f507533e985f7f68
dang <dang.nguyen14082002@gmail.com>
2024-05-29 07:57:48 +0700

    .

--- trainer/dpo.py
+++ trainer/dpo.py
@@ -171,10 +171,6 @@ def main():
     trainer.save_model()  # Saves the tokenizer too for easy upload
 
     metrics = train_result.metrics
-    max_train_samples = (
-        data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
-    )
-    metrics["train_samples"] = min(max_train_samples, len(train_dataset))
 
     with save_context:
         trainer.log_metrics("train", metrics)
