2f72b840031d1e8f433d4da060aabbc617c6467f
dang <dang.nguyen14082002@gmail.com>
2024-05-31 00:27:14 +0700

    .

--- utils/generate.py
+++ utils/generate.py
@@ -38,41 +38,41 @@ else:
 if args.adapter_name_or_path:
     model.load_adapter(args.adapter_name_or_path)
 
+accelerator.wait_for_everyone()
+
 with jsonlines.open(args.test_filename, mode="r") as fr, jsonlines.open(args.output_filename, mode="w") as fw:
     text = []
     for line in fr:
         text.append(line)
     
-accelerator.wait_for_everyone()
+    rougeL_pre, rougeL_rec, rougeL_f1 = [], [], []
+    with accelerator.split_between_processes(text) as prompts:
+        for i in tqdm(range(0, len(text), args.gen_batch_size), desc ="Generating:"):
+            inputs = [tokenizer.bos_token + make_prompt(text[i + ii]["context"], text[i + ii]["answer"], last_space=False) for ii in range(min(args.gen_batch_size, len(text) - i))] 
+            input_ids = tokenizer(inputs, max_length=args.max_seq_length, padding=True, truncation=True, return_tensors="pt").to(device)
+            preds = model.generate(
+                **input_ids,
+                min_new_tokens=args.min_new_tokens,
+                max_new_tokens=args.max_new_tokens,
+                do_sample=args.do_sample,
+                temperature=args.temperature,
+                top_k=args.top_k,
+                top_p=args.top_p,
+            )
+            outputs = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
-rougeL_pre, rougeL_rec, rougeL_f1 = [], [], []
-with accelerator.split_between_processes(text) as prompts:
-    for i in tqdm(range(0, len(text), args.gen_batch_size), desc ="Generating:"):
-        inputs = [tokenizer.bos_token + make_prompt(text[i + ii]["context"], text[i + ii]["answer"], last_space=False) for ii in range(min(args.gen_batch_size, len(text) - i))] 
-        input_ids = tokenizer(inputs, max_length=args.max_seq_length, padding=True, truncation=True, return_tensors="pt").to(device)
-        preds = model.generate(
-            **input_ids,
-            min_new_tokens=args.min_new_tokens,
-            max_new_tokens=args.max_new_tokens,
-            do_sample=args.do_sample,
-            temperature=args.temperature,
-            top_k=args.top_k,
-            top_p=args.top_p,
-        )
-        outputs = tokenizer.batch_decode(preds, skip_special_tokens=True)
-
-        for ii in range(min(args.gen_batch_size, len(text) - i)):
-            line_ = {}
-            line_["context"] = text[i + ii]["context"]
-            line_["answer"] = text[i + ii]["answer"]
-            line_["target"] = text[i + ii]["question"]
-            if not model.config.is_encoder_decoder:
-                pred = outputs[ii].split("### Câu hỏi: ")
-                if len(pred) == 2:
-                    line_["pred"] = pred[1]
-                    fw.write(line_)
+            for ii in range(min(args.gen_batch_size, len(text) - i)):
+                line_ = {}
+                line_["context"] = text[i + ii]["context"]
+                line_["answer"] = text[i + ii]["answer"]
+                line_["target"] = text[i + ii]["question"]
+                if not model.config.is_encoder_decoder:
+                    pred = outputs[ii].split("### Câu hỏi: ")
+                    if len(pred) == 2:
+                        line_["pred"] = pred[1]
+                        fw.write(line_)
+                    else:
+                        pass
                 else:
-                    pass
-            else:
-                line_["pred"] = outputs[ii]
-                fw.write(line_)
+                    line_["pred"] = outputs[ii]
+                    fw.write(line_)
