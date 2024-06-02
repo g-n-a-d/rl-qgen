8787534c4bb55106e851dc1838bd3565e1fb1170
dang <dang.nguyen14082002@gmail.com>
2024-06-02 16:07:40 +0700

    .

--- train_clm.sh
+++ train_clm.sh
@@ -17,25 +17,24 @@ accelerate launch \
     --validation_file ./data/processed/dev.jsonl \
     --max_source_length 1024 \
     --chat_template vistral \
-    --response_mark "[\INST]" \
+    --response_mark "[/INST]" \
     --output_dir ./outputs/ \
     --do_train \
     --do_eval \
     --evaluation_strategy steps \
-    --eval_steps 100 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 16 \
-    --learning_rate 2e-4 \
-    --num_train_epochs 30 \
+    --eval_steps 50 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 5 \
     --lr_scheduler_type cosine \
     --warmup_ratio 0.1 \
     --logging_strategy steps \
-    --logging_steps 25 \
+    --logging_steps 10 \
     --save_strategy steps \
-    --save_step 100 \
-    --save_total_limit 30 \
+    --save_step 50 \
+    --save_total_limit 17 \
     --load_best_model_at_end True \
     --bf16 \
-    --bfp16_full_eval \
     --report_to none
