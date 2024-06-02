a2704d2b197ddf85f132a7943d15b075cd262246
dang <dang.nguyen14082002@gmail.com>
2024-06-02 22:50:04 +0700

    .

--- utils/generate.py
+++ utils/generate.py
@@ -1,7 +1,13 @@
 import torch
-from transformers import AutoConfig, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    BitsAndBytesConfig
+)
 from peft import LoraConfig, get_peft_model
-from accelerate import Accelerator
+from accelerate import Accelerator, PartialState
 import jsonlines
 import argparse
 from tqdm import tqdm
@@ -12,6 +18,7 @@ from data_utils import make_prompt
 parser = argparse.ArgumentParser()
 parser.add_argument("--test_filename", type=str, help="Test file")
 parser.add_argument("--model_name_or_path", type=str, help="Model")
+parser.add_argument("--torch_dtype", type=str, default="float32", help="Dtype")
 parser.add_argument("--adapter_name_or_path", type=str, default=None, help="Adapter")
 parser.add_argument("--token", type=str, default=None, help="Token")
 parser.add_argument("--gen_batch_size", type=int, default=8, help="Evaluation batch size")
@@ -27,18 +34,27 @@ args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-accelerator = Accelerator()
+distributed_state = PartialState()
 
-tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-config = AutoConfig.from_pretrained(args.model_name_or_path)
+tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, token=args.token)
+config = AutoConfig.from_pretrained(args.model_name_or_path, token=args.token)
 if not config.is_encoder_decoder:
-    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path).to(device)
+    nf4_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=args.torch_dtype
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_name_or_path,
+        token=args.token,
+        torch_dtype=getattr(torch, args.torch_dtype),
+        # quantization_config=nf4_config
+    ).to(distributed_state.device)
 else:
-    model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name_or_path).to(device)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name_or_path)
 if args.adapter_name_or_path:
     model.load_adapter(args.adapter_name_or_path)
-
-accelerator.wait_for_everyone()
 
 with jsonlines.open(args.test_filename, mode="r") as fr, jsonlines.open(args.output_filename, mode="w") as fw:
     text = []
@@ -46,9 +62,9 @@ with jsonlines.open(args.test_filename, mode="r") as fr, jsonlines.open(args.out
         text.append(line)
     
     rougeL_pre, rougeL_rec, rougeL_f1 = [], [], []
-    with accelerator.split_between_processes(text) as prompts:
-        for i in tqdm(range(0, len(text), args.gen_batch_size), desc ="Generating:"):
-            inputs = [tokenizer.bos_token + make_prompt(text[i + ii]["context"], text[i + ii]["answer"], last_space=False) for ii in range(min(args.gen_batch_size, len(text) - i))] 
+    with distributed_state.split_between_processes(text) as text_:
+        for i in tqdm(range(0, len(text_), args.gen_batch_size), desc ="Generating:"):
+            inputs = [tokenizer.bos_token + make_prompt(text_[i + ii]["context"], text_[i + ii]["answer"], last_space=False) for ii in range(min(args.gen_batch_size, len(text_) - i))] 
             input_ids = tokenizer(inputs, max_length=args.max_seq_length, padding=True, truncation=True, return_tensors="pt").to(device)
             preds = model.generate(
                 **input_ids,
@@ -61,11 +77,11 @@ with jsonlines.open(args.test_filename, mode="r") as fr, jsonlines.open(args.out
             )
             outputs = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
-            for ii in range(min(args.gen_batch_size, len(text) - i)):
+            for ii in range(min(args.gen_batch_size, len(text_) - i)):
                 line_ = {}
-                line_["context"] = text[i + ii]["context"]
-                line_["answer"] = text[i + ii]["answer"]
-                line_["target"] = text[i + ii]["question"]
+                line_["context"] = text_[i + ii]["context"]
+                line_["answer"] = text_[i + ii]["answer"]
+                line_["target"] = text_[i + ii]["question"]
                 if not model.config.is_encoder_decoder:
                     pred = outputs[ii].split("### Câu hỏi: ")
                     if len(pred) == 2:
